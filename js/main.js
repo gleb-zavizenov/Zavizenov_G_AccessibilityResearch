c9e6a59bb34d54df3702f5578fece5a15c56c0cd
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2020-04-01 11:11:54 -0400

    Added files

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ const vm = new Vue({
           {name: 'Once Upon A Time In Hollywod',image: 'once.jpg', video: 'once.mp4', subtitles:'sub-once.mp4', transcript: 'once.pdf'},
           {name: 'Never Gona Give You Up',image: 'never-give-up.jpg', video: 'never-give-up.mp4', subtitles:'sub-never-give-up.mp4', transcript: 'never-give-up.pdf'}
       ],
-      selectedIndex : 0
+      selectedIndex : 0,
+      captions: false
     },
 
     methods:{
@@ -23,6 +24,7 @@ const vm = new Vue({
             let video = document.querySelector(".video-lightbox-container video");
             this.pauseVideo();
             this.unmuteVideo();
+            this.captions = false;
             video.currentTime = 0;
             video.muted = false;
         },
@@ -43,26 +45,35 @@ const vm = new Vue({
             document.querySelector(".video-lightbox-container video").muted = true;
             document.querySelector(".sound-on").style.display = "none";
             document.querySelector(".sound-off").style.display = "block";
+            document.querySelector(".sound-range").value = 0;
         },
 
         unmuteVideo(){
             document.querySelector(".video-lightbox-container video").muted = false;
             document.querySelector(".sound-on").style.display = "block";
             document.querySelector(".sound-off").style.display = "none";
+            document.querySelector(".sound-range").value = 1;
         },
 
         showSubs(){
             let video = document.querySelector(".video-lightbox-container video");
+            this.pauseVideo();
+            let currentTime = video.currentTime;
             let captionsBtn = document.querySelector(".captions-btn");
-            if(video.src = `./video/subs-${this.content[this.selectedIndex]['video']}`){
+            if(this.captions){
                 video.src = `./video/${this.content[this.selectedIndex]['video']}`;
+                this.captions = false;
+                video.currentTime = currentTime;
                 captionsBtn.style.backgroundColor = "white";
                 captionsBtn.style.color = "black";
-            } else {
-                video.src = `./video/subs-${this.content[this.selectedIndex]['video']}`;
+            }else{
+                video.src = `./video/${this.content[this.selectedIndex]['subtitles']}`;
+                this.captions = true;
+                video.currentTime = currentTime;
                 captionsBtn.style.backgroundColor = "green";
                 captionsBtn.style.color = "white";
             }
+            this.playVideo();
         },
 
         displayTime(){
@@ -76,6 +87,10 @@ const vm = new Vue({
             let video = document.querySelector(".video-lightbox-container video");
             let newTime = (e.offsetX/progressBar.offsetWidth) * video.duration;
             video.currentTime = newTime;
+        },
+
+        changeVolume(e){
+            document.querySelector(".video-lightbox-container video").volume = e;
         }
     }
 });
